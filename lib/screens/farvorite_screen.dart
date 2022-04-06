08356a016858249d529f597daf5abb87229d7324
PhaNith <nochphanith@gmail.com>
2022-04-06 22:34:45 +0700

    added favorite and profile screen

--- lib/screens/farvorite_screen.dart
+++ lib/screens/farvorite_screen.dart
@@ -5,6 +5,96 @@ class FavoriteScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              Row(
+                children: const [
+                  Text(
+                    'Activity',
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: const [
+                  Text('New', style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              fovoriteItemWidget(),
+              fovoriteItemWidget(),
+              fovoriteItemWidget(),
+              fovoriteItemWidget(),
+              Row(
+                children: const [
+                  Text('Suggested for youu',
+                      style: TextStyle(fontWeight: FontWeight.bold)),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              fovoriteItemWidget(),
+              fovoriteItemWidget(),
+              fovoriteItemWidget(),
+              fovoriteItemWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class fovoriteItemWidget extends StatelessWidget {
+  const fovoriteItemWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Row(
+        children: [
+          const CircleAvatar(
+            foregroundImage: NetworkImage(
+                'https://i.pinimg.com/736x/71/80/fe/7180fec2184747e8034b2988752ea7da.jpg'),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.6,
+            child: Row(
+              children: const [
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  'Chelinchelin ',
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'started following you.',
+                  style: TextStyle(fontSize: 10),
+                ),
+                Text(
+                  '3d',
+                  style: TextStyle(fontSize: 10),
+                ),
+              ],
+            ),
+          ),
+          Expanded(child: TextButton(onPressed: () {}, child: Text('Follow')))
+        ],
+      ),
+    );
   }
 }
--- lib/screens/person_scrren.dart
+++ lib/screens/person_scrren.dart
@@ -5,6 +5,137 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Manel_rzl'),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.menu))
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 52,
+                        backgroundColor: Colors.grey,
+                        child: CircleAvatar(
+                          radius: 50,
+                          foregroundImage: NetworkImage(
+                            'https://i.pinimg.com/280x280_RS/50/65/03/506503e39b5285f7831cb02d8e8f9e1d.jpg',
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Column(
+                            children: const [
+                              Text(
+                                '44',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 22),
+                              ),
+                              Text('Posts'),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            children: const [
+                              Text(
+                                '44',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 22),
+                              ),
+                              Text('Followers'),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            children: const [
+                              Text(
+                                '44',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 22),
+                              ),
+                              Text('Following'),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text('Hii !\'m Manaaal '),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text('1999* | Morocco'),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text('Creator of @__sciarpa'),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text('Soon to of everywhere'),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: OutlinedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0))),
+                          ),
+                          child: const Text('Edit Profile'),
+                          onPressed: () {},
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: OutlinedButton(
+                          style: ButtonStyle(
+                            shape: MaterialStateProperty.all(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0))),
+                          ),
+                          child: const Text('Promotion'),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )),
+      ),
+    );
   }
 }
